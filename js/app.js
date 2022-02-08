7342f75756e99ad0de17e85d2f5cd67fffd5a815
unknown <cristianaaguirre0@gmail.com>
2022-02-08 19:59:55 -0300

    agregando una animacion

--- js/app.js
+++ js/app.js
@@ -1,7 +1,14 @@
 const abrirMenu = document.querySelector('.menu');
 const menuBars = document.querySelector('.navbar__nav')
-// const navOption = document.querySelectorAll('a.nav__option');
+const navOption = document.querySelectorAll('a.nav__option');
 
 abrirMenu.addEventListener('click', () => {
     menuBars.classList.toggle('navbar__nav--modifier');
 })
+
+
+for (let i = 0; i < navOption.length ; i++) {
+    navOption[i].addEventListener('click', () => {
+        menuBars.classList.remove('navbar__nav--modifier');
+    })
+};
