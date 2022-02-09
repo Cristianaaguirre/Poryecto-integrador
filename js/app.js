0db2d9e6e81f714890a7b2f2b49c1f0513ecd60a
unknown <cristianaaguirre0@gmail.com>
2022-02-08 22:49:03 -0300

    agregando ultimos detalles al proyecto

--- js/app.js
+++ js/app.js
@@ -1,14 +1,18 @@
-const abrirMenu = document.querySelector('.menu');
+const openMenu = document.querySelector('.menu');
 const menuBars = document.querySelector('.navbar__nav')
 const navOption = document.querySelectorAll('a.nav__option');
+const backToTop = document.querySelector('.go-up__img');
 
-abrirMenu.addEventListener('click', () => {
+openMenu.addEventListener('click', () => {
     menuBars.classList.toggle('navbar__nav--modifier');
+});
+backToTop.addEventListener('click', () => {
+     backToTop.classList.remove('go-up__img--modifier')
 })
-
 
 for (let i = 0; i < navOption.length ; i++) {
     navOption[i].addEventListener('click', () => {
         menuBars.classList.remove('navbar__nav--modifier');
+        backToTop.classList.add('go-up__img--modifier');
     })
 };
